89fe73902befad2dafa9b6e8a74486c0c9545435
XiaoyuYe123 <xiaoyu24@intl.zju.edu.cn>
2025-03-14 09:19:11 +0800

    1

--- PRACTICAL4/bmi_calculator.py
+++ PRACTICAL4/bmi_calculator.py
@@ -1,11 +1,18 @@
+#function: BMI Calculator:Show people's BMI and whether they are overweight, underweight or normal weight
+#BMI=weight(kg)/height(m)^2
+#Input weight and height
 weight = float(input('Enter your weight in kg:'))
 height = float(input('enter your height in m:'))
+#clculator BMI
 BMI = weight /(height**2)
+#If BMI<18.5, State=underweight
+#If 18.5<=BMI<25, State=normal weight
+#If BMI>=25, State=overweight
 print('Your BMI is', str(BMI))
 if BMI < 18.5:
     print('You are underweight')
 elif BMI > 30:
     print('You are obese')
 else:
+    #Print BMI and State
     print('You are normal wweight')
-56
